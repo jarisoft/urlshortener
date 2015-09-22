6a62ef793cc8cd9c45a4eb0c7dbc19ddd01179f3
Jakob <info@jarisoft.eu>
2015-09-22 22:30:02 +0100

    Apache License 2.0 add to project

--- README.md
+++ README.md
@@ -5,7 +5,9 @@ This project was build using MVC pattern and uses some other design patterns suc
 
 # What it does
 This service provides the opportunity to create shorter URLs by a given url.
-You can look up a url and see if it is already used as target url or as short url. 
+You can look up an URL and see if it is already used as target URL or as short url. 
+This service also checks if a given URL exists and if its a redirecting target. 
+
 
 The front-end is very simple. It uses bootstrap and jquery. The focus was more on the back-end side. 
 
--- src/Jarisoft/Controller/Controller.php
+++ src/Jarisoft/Controller/Controller.php
@@ -1,4 +1,19 @@
 <?php
+/*
+ * Copyright 2015 Jakob
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 namespace Jarisoft\Controller;
 
 require_once 'src/Jarisoft/Resources/Validator.php';
@@ -76,7 +91,7 @@ class Controller
         curl_exec($ch);
         $returnCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
         curl_close($ch);
-       
+        
         if ($returnCode < 400) {
             return true;
         } else {
@@ -163,9 +178,9 @@ class Controller
      * If $newSessionID === true the parameter
      * $_SESSION['random_key'] is set to a random string if this value has not been set
      * before.
-     * 
+     *
      * @param bool $newSessionID            
-     * 
+     *
      * @return the $_SESSION variale.
      */
     public function getSession($newSessionID)
